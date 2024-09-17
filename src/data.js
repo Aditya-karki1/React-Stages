2da7c477bf963e6e2b9c7245b21d33b847d71a4c
Aditya-karki1 <adityakarki20247505@gmail.com>
2024-09-17 23:42:01 +0530

    this is how stages work

---
+++ src/data.js
@@ -0,0 +1,67 @@
+const data = [
+    {
+        id: 1,
+        name: "Agra",
+        info : "Agra is a city in Norther India , known for the iconic Taj mahal , " ,
+        image: "https://cdn.thecodehelp.in/Agra.jpeg",
+        price : "35,450"
+    }, 
+    {
+        id: 2,
+        name: "Jaipur",
+        info : "Agra is a city in Norther India , known for the iconic Taj mahal , " ,
+        image: "https://cdn.thecodehelp.in/Jaipur.jpeg",
+        price : "35,450"
+    },
+    {
+        id: 3,
+        name: "GOA",
+        info : "Agra is a city in Norther India , known for the iconic Taj mahal , " ,
+        image: "https://cdn.thecodehelp.in/Goa.jpeg",
+        price : "35,450"
+    },
+    {
+        id: 5 ,
+        name: "Agra",
+        info : "Agra is a city in Norther India , known for the iconic Taj mahal , " ,
+        image: "https://cdn.thecodehelp.in/Agra.jpeg",
+        price : "35,450"
+    },
+    {
+        id: 5,
+        name: "Jaipur",
+        info : "Agra is a city in Norther India , known for the iconic Taj mahal , " ,
+        image: "https://cdn.thecodehelp.in/Agra.jpeg",
+        price : "35,450"
+    },
+    {
+        id: 6,
+        name: "GOA",
+        info : "Agra is a city in Norther India , known for the iconic Taj mahal , " ,
+        image: "https://cdn.thecodehelp.in/Goa.jpeg",
+        price : "35,450"
+    },
+    {
+        id: 7,
+        name: "Agra",
+        info : "Agra is a city in Norther India , known for the iconic Taj mahal , " ,
+        image: "https://cdn.thecodehelp.in/Agra.jpeg",
+        price : "35,450"
+    },
+    {
+        id: 8,
+        name: "Jaipur",
+        info : "Agra is a city in Norther India , known for the iconic Taj mahal , " ,
+        image: "https://cdn.thecodehelp.in/Agra.jpeg",
+        price : "35,450"
+    },
+    {
+        id: 9,
+        name: "GOA",
+        info : "Agra is a city in Norther India , known for the iconic Taj mahal , " ,
+        image: "https://cdn.thecodehelp.in/Goa.jpeg",
+        price : "35,450"
+    }
+]
+
+export default data ;
